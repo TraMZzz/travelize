bdd11fb6fd5be4ba154e622d6d26ff0300e9acd9
tramzzz <steh44@gmail.com>
2016-06-04 09:07:35 +0300

    proc

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,22 +1,21 @@
 """
-WSGI config for travelize project.
-
+WSGI config for go_green project.
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
 named ``application``. Django's ``runserver`` and ``runfcgi`` commands discover
 this application via the ``WSGI_APPLICATION`` setting.
-
 Usually you will have the standard Django WSGI application here, but it also
 might make sense to replace the whole Django WSGI application with a custom one
 that later delegates to the Django one. For example, you could introduce WSGI
 middleware here, or combine a Django application with an application of another
 framework.
-
 """
 import os
 
 
 from django.core.wsgi import get_wsgi_application
+from whitenoise.django import DjangoWhiteNoise
+
 
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
@@ -30,6 +29,9 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings.production")
 # setting points here.
 application = get_wsgi_application()
 
+# Use Whitenoise to serve static files
+# See: https://whitenoise.readthedocs.org/
+application = DjangoWhiteNoise(application)
 
 # Apply WSGI middleware here.
 # from helloworld.wsgi import HelloWorldApplication
