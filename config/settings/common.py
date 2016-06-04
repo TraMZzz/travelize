8dc6e975790f589288ce8ef883d75e7ddef609fa
tramzzz <steh44@gmail.com>
2016-06-04 09:42:26 +0300

    test

--- config/settings/common.py
+++ config/settings/common.py
@@ -181,7 +181,7 @@ STATIC_URL = '/static/'
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#std:setting-STATICFILES_DIRS
 STATICFILES_DIRS = (
-    str(APPS_DIR.path('static')),
+    str(ROOT_DIR('static')),
 )
 
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/staticfiles/#staticfiles-finders
